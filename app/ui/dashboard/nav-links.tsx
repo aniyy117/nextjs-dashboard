36a013d86d8e9b91f1a09acdd20454ddeb852a91
aniket0711 <aniket.jadhav@techchefz.com>
2024-01-22 15:59:51 +0530

    Completed Chapter 8

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,7 +9,6 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -23,7 +22,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const  pathname = usePathname();
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
